4f3b15d492e31e726e986f6d06969bfe03201c59
matheusTA <matrajano12@gmail.com>
2025-06-13 23:27:47 -0300

    Adiciona estrutura inicial do módulo de faturas, incluindo configuração do Docker, arquivos de migração para o banco de dados PostgreSQL, e integração com RabbitMQ. Cria arquivos de configuração, como .dockerignore, .editorconfig, package.json e tsconfig.json, além de implementar a lógica de conexão com o banco de dados e gerenciamento de filas.

---
+++ app-invoices/src/db/schemas/index.ts
@@ -0,0 +1,5 @@
+import { invoices } from "./invoices.ts";
+
+export const schemas = {
+  invoices,
+}
---
+++ app-invoices/src/http/server.ts
@@ -0,0 +1,21 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+
+import '../broker/subscriber.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.get('/health', () => 'OK');
+
+
+app.listen({host: '0.0.0.0', port: 3334}).then(() => {
+  console.log('[INVOICES] HTTP server running!')
+})
